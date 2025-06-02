4fb01a6b76c4ebd4d5b3b1bd241a70cc76bf38c0
ejminguez <ebminguez@up.edu.ph>
2025-06-02 21:47:40 +0800

    fix: root rendering

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@
 import { render } from "solid-js/web";
 import routes from "./routes/routes";
 import { Router } from "@solidjs/router";
-import App from "./App";
 
-const root = document.getElementById("app");
-
-render(() => <Router>{routes}</Router>, document.getElementById("root"));
+render(
+  () => <Router>{routes}</Router>,
+  document.getElementById("root") as HTMLElement,
+);
